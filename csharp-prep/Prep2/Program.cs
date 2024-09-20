71c4ba6dbed580f4fdf2abf3bb69b8ce34e93eca
JairAnaya <jair.anaya.rodriguez@gmail.com>
2024-09-19 18:26:36 -0600

    Prep 1 and prep 2 changed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        float grade = float.Parse(userInput);
+        string letter = " ";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class, you can do it better next time");
+        }
+        Console.WriteLine($"Grade: '{letter}'");
     }
 }
