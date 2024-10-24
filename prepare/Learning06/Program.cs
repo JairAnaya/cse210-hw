20b1cb165b8fa1864ebb14e469cdb23f55fd9b82
JairAnaya <jair.anaya.rodriguez@gmail.com>
2024-10-23 18:07:10 -0600

    Learning06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square mySquare = new Square("red", 1.5);
+        shapes.Add(mySquare);
+        Console.WriteLine(mySquare.GetColor());
+        Console.WriteLine(mySquare.GetArea());
+
+        Rectangle myRectangle = new Rectangle("blue", 2, 1);
+        shapes.Add(myRectangle);
+        Console.WriteLine(myRectangle.GetColor());
+        Console.WriteLine(myRectangle.GetArea());
+
+        Circle myCircle = new Circle("black", 2.3);
+        shapes.Add(myCircle);
+        Console.WriteLine(myCircle.GetColor());
+        Console.WriteLine(myCircle.GetArea());
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
